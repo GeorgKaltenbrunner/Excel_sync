b790999cd61903775fa5bfd8339baabf00700546
Georg Kaltenbrunner <kaltenbrunnergeorg@googlemail.com>
2022-11-23 11:36:19 +0100

    Readme

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Excel_sync
 Concat specific information from different excel files
+
+## Background
+The problem faced was that about 500 Excel with a similar structure were given. From each excel the information needed
+could be specified on 6 columns from each the mean was needed.
+
+
+## Code explanation
+- The first argument is the seperator, to split the filenmane correctly.
+- The second argument is for the column name
+
---
+++ main.py
@@ -0,0 +1,46 @@
+import glob
+import pandas as pd
+
+
+def get_excel_data(seperator, regre_method):
+    # DataFrame to store all the Data from the Excel Files
+    validation_df = pd.DataFrame()
+
+    for file in glob.glob('*.xlsx'):
+        filename = file
+
+        # Read Excelfile
+        df = pd.read_excel(filename)
+
+        # Get data from Excel -> Here specify the columns needed
+        early_orders = df.loc[df['Replication_Nr'] == 'Mean']['Early Ratio'].item()
+        tardy_orders = df.loc[df['Replication_Nr'] == 'Mean']['Tardy Ratio'].item()
+        earliness = df.loc[df['Replication_Nr'] == 'Mean']['Earliness'].item()
+        tardiness = df.loc[df['Replication_Nr'] == 'Mean']['Tardiness'].item()
+        avg_sftt = df.loc[df['Replication_Nr'] == 'Mean']['Avg_SFTT'].item()
+        prognose_mean = df['Prognosegüte'].mean()
+
+        # Extract name for column
+        file_split = filename.split(".xlsx")
+        file_split_name_column = file_split[0].split(seperator)
+        file_column = file_split_name_column[0] + regre_method + file_split_name_column[1]
+        print(file_split_name_column)
+
+        # New Dataframe -> to store the needed information
+        df_file = pd.DataFrame({'Early_Orders_Ratio': early_orders,
+                                'Tardy_Orders_Ratio': tardy_orders,
+                                'Earliness': earliness,
+                                'Tardiness': tardiness,
+                                'Avg_SFTT': avg_sftt,
+                                'Prognosegüte': prognose_mean}, index=[file_column])
+
+
+
+        validation_df = pd.concat([validation_df, df_file])
+
+        print()
+
+
+
+
+get_excel_data("_EXP_ALL_DEMAND_", "_EXP_ALL_DEMAND_")
